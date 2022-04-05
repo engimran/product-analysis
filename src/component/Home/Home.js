621787324a725ec43f89e85fb8ec13bdd78e05ba
Imran Hossen <imranhossen20@gmail.com>
2022-04-05 15:50:25 +0600

    All review section done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h2>home page</h2>
+        <div className='home-container'>
+            <div>
+                <h2>Best IT Equipment</h2>
+            </div>
+            <div>
+                <h2>Best mouse</h2>
+
+                <img src="Image.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Reviews.css'
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+
     return (
-        <div>
-            <h2>Review</h2>
+        <div className='review-style'>
+            {
+                reviews.map(review => <div className='border-style' >
+                    <img src={review.picture} alt="" />
+                    <p>Reviewer Name: {review.name}</p>
+                    <p>Comments: {review.comments}</p>
+                    <p>Ratings: {review.rating}</p>
+                </div>
+                )
+            }
         </div>
     );
 };
